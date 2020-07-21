eadaead62961c132023a0f9167f8e611ca0d65b7
AdiletAldazharbekov <aldajarbekov@gmail.com>
2020-07-22 00:29:55 +0600

    Добавил конструктов в класс Студент

--- Student.cs
+++ Student.cs
@@ -6,5 +6,22 @@ namespace Assignment11
 {
     class Student
     {
+        string Name;
+        string Surname;
+        string Group;
+        string Faculty;
+        int Course;
+        int Age;
+
+        public Student(string name, string surname, string group, string faculty, int course, int age)
+        {
+            Name=name;
+            Surname=surname;
+            Group = group;
+            Faculty=faculty;
+            Course=course;
+            Age=age;
+        }
+
     }
 }
